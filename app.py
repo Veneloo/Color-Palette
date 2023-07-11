4f4dc1b1f1ce18a3711fb64219f233e6e94428cd
Semhar Haile <shaile03@terpmail.umd.edu>
2023-07-11 17:04:47 -0400

    working on layout.html template

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ def welcome_page():
     
 @app.route("/random")
 def random_page():
-    return render_template('random_page.html', subtitle='Random Palette Generator', text='This is the Random Palette Generator')
+    return render_template('random.html', subtitle='Random Palette Generator', text='This is the Random Palette Generator')
 
 @app.route("/personalized")
 def personalized_page():
-    return render_template('personalized_page.html', subtitle='Personalized Palette Generator', text='This is the Personalized Palette Generator')
+    return render_template('personalized.html', subtitle='Personalized Palette Generator', text='This is the Personalized Palette Generator')
 
 @app.route("/history")
 def history_page():
-    return render_template('history_page.html', subtitle='History', text='This is the History page')
+    return render_template('history.html', subtitle='History', text='This is the History page')
 
 @app.route("/favorites")
 def favorites_page():
-    return render_template('favorites_page.html', subtitle = 'Favorites', text = 'This is the Favorites page')
+    return render_template('favorites.html', subtitle = 'Favorites', text = 'This is the Favorites page')
 
   
 if __name__ == '__main__':               
