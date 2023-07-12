8613a722ae1e2b8895d9791fcd2a355200e8aee6
Axel Cazorla <axel.cazorla10@gmail.com>
2023-07-12 00:07:17 -0400

    Updated Files

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)                    
 
 @app.route("/")
-@app.route("welcome")
+@app.route("/welcome")
 def welcome_page():
     return render_template('welcome.html', subtitle='Welcome Page', text='This is the welcome page')
     
