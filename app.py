ef7f57911945b08977abc3e719ea6ab321885182
Semhar Haile <shaile03@terpmail.umd.edu>
2023-07-12 17:30:53 -0400

    changes

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def random_page():
          for i in range(5):
              color = response['colors'][i]['hex']['value']
              colors.append(color)
-    return render_template('random.html', subtitle='Random Palette Generator', text='This is the Random Palette Generator', colors = colors)
+    return render_template('random.html', subtitle='Random Palette Generator', text='This is the Random Palette Generator')
 
 
 @app.route('/result', methods=['POST'])
