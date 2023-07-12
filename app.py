0dbdb421865db7f83e82e4e174f877d72ae6244a
Semhar Haile <shaile03@terpmail.umd.edu>
2023-07-12 16:45:34 -0400

    changes

--- app.py
+++ app.py
@@ -13,22 +13,6 @@ def welcome_page():
 
 @app.route("/random")
 def random_page():
-    # if request.method == 'POST':
-    #     # Generate random RGB values
-    #     rand_color = random.choices(range(256), k=3)
-    #     rgb_vals = str(rand_color[0]) + ',' + str(rand_color[1]) + ',' + str(rand_color[2])
-
-    #     # Make API request to generate color palette
-    #     url = 'https://www.thecolorapi.com/scheme?rgb=' + rgb_vals
-    #     response = requests.get(url).json()
-  # Extract color values from the API response
-    #     colors = []
-    #     for i in range(5):
-    #         color = response['colors'][i]['hex']['value']
-    #         colors.append(color)
-
-    #     return render_template('random.html', subtitle='Random Palette Generator', text='This is the Random Palette Generator', colors=colors)
-
     return render_template('random.html', subtitle='Random Palette Generator', text='This is the Random Palette Generator')
 
 
