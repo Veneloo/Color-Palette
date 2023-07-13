a1b7d5298ccb0c145c0123c75f6653b44037c7a8
Celestenc <celestencarrasco@gmail.com>
2023-07-13 12:46:51 -0400

    Webstie testing

---
+++ tests/test_basic.py
@@ -0,0 +1,42 @@
+import unittest, sys
+
+sys.path.append('../Music-Palette') # imports python file from parent directory
+from app import app #imports flask app object
+
+class BasicTests(unittest.TestCase):
+
+    # executed prior to each test
+    def setUp(self):
+        self.app = app.test_client()
+
+    ###############
+    #### tests ####
+    ###############
+
+    def register_page(self):
+        response = self.app.get('/register', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def login(self):
+        response = self.app.get('/login', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def logout(self):
+        response = self.app.get('/logout', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def welcome_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def random(self):
+        response = self.app.get('/random', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def clear_entries(self):
+        response = self.app.get('/history,' follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
